758b41371a99f16c4a1656d8c834451fc4a0f5bf
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-14 11:55:58 -0700

    sum of digits codewars

---
+++ sumofdigits.js
@@ -0,0 +1,6 @@
+//Given 'n', take the sum of the digts of 'n'. If that value has more than
+//one digit, continue reducing in this way until a single-digit number
+//is produced. The input will be a non-negative integer.
+//ex. 16 --> 1 + 6 = 7
+// 942 --> 9 + 4 + 2 = 15 --> 1 + 5 = 6
+
