d6081ff1b072cf421478e30febf83ef804ec119b
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-20 16:32:20 -0700

    string hash array codewars

--- hasharray.js
+++ hasharray.js
@@ -6,3 +6,11 @@
 //example: list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
 // returns 'Bart, Lisa & Maggie'
 
+//list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// returns 'Bart & Lisa'
+
+list([ {name: 'Bart'} ])
+// returns 'Bart'
+
+list([])
+// returns ''
