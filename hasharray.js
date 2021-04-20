d58d2f7153c93351c2046fd5ca6f10fad6b6b915
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-20 16:31:57 -0700

    string hash array codewars

--- hasharray.js
+++ hasharray.js
@@ -3,4 +3,6 @@
 //Return: a string formatted as a list of names separated by commas 
 //except for the last two names, which should be separated by an ampersand.
 
+//example: list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
 
