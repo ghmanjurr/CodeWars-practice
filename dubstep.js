830294e4852ac5fb48114e4c7fbb0ff063d84b4a
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-21 17:36:33 -0700

    dubstep codewars

--- dubstep.js
+++ dubstep.js
@@ -21,5 +21,16 @@
 //what was the initial song that Polycarpus remixed. 
 //Help Jonny restore the original song.
 
+//INPUT: 
+//The input consists of a single non-empty string, consisting 
+//only of uppercase English letters, the string's length doesn't 
+//exceed 200 characters
+
+//OUTPUT
+//Return the words of the initial song that Polycarpus used to 
+//make a dubsteb remix. Separate the words with a space.
+
+
+
 
 
