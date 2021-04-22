70c01ca2d40cb458997f3602d60216343d97aec9
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-21 17:35:14 -0700

    dubstep codewars

--- dubstep.js
+++ dubstep.js
@@ -11,3 +11,15 @@
 //(at least one between any pair of neighbouring words), 
 //and then the boy glues together all the words, including "WUB", 
 //in one string and plays the song at the club.
+
+//For example, a song with words "I AM X" can transform into a 
+//dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform 
+//into "WUBWUBIAMWUBX".
+
+//Recently, Jonny has heard Polycarpus's new dubstep track, 
+//but since he isn't into modern music, he decided to find out 
+//what was the initial song that Polycarpus remixed. 
+//Help Jonny restore the original song.
+
+
+
