de355ef00507fdff0383ff594c5310adafe972d9
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-23 23:42:53 -0700

    string to camelcase codewars

--- camelcase.js
+++ camelcase.js
@@ -2,3 +2,8 @@
 //delimited words into camel casing. The first word within the output 
 //should be capitalized only if the original word was capitalized 
 //(known as Upper Camel Case, also often referred to as Pascal case).
+
+//Examples
+//"the-stealth-warrior" gets converted to "theStealthWarrior"
+//"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
