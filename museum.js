e2c503d856bd3d8f43a7d8c2f6c57f76c1880572
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-25 22:13:10 -0700

    museum codewars

--- museum.js
+++ museum.js
@@ -9,4 +9,11 @@
 //important fair, so she asks you to write a program that tells her the 
 //ratings of the items after one removed the lowest one. Fair enough.
 
+//Task
+//Given an array of integers, remove the smallest value. 
+//Do not mutate the original array/list. 
+//If there are multiple elements with the same value, 
+//remove the one with a lower index. If you get an empty array/list, 
+//return an empty array/list.
 
+//Don't change the order of the elements that are left.
