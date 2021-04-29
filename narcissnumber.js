a849f6e01a8d775323b8c9bab6d65ab892cfb379
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-04-28 19:58:24 -0700

    narcissistic number codewars

--- narcissnumber.js
+++ narcissnumber.js
@@ -9,4 +9,10 @@
 //  1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
 
 //The Challenge:
-//
+//Your code must return true or false depending upon whether 
+//the given number is a Narcissistic number in base 10.
+
+//Error checking for text strings or other invalid inputs is not required, 
+//only valid positive non-zero integers will be passed into the function.
+
+
