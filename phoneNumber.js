e6b978012b78f20fbb0d9025d7c893354330fbea
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-04 18:02:44 -0700

    phone number codewars

--- phoneNumber.js
+++ phoneNumber.js
@@ -4,3 +4,6 @@
 //(between 0 and 9), that returns a string of those numbers 
 //in the form of a phone number.
 
+//example:
+//createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
+    // => returns "(123) 456-7890"
