22b8d7be09c9a462a26a0007e00fa1aadef09cf2
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-08 00:19:33 -0700

    square of squares codewars

--- squareOfSquares.js
+++ squareOfSquares.js
@@ -18,3 +18,6 @@
 //that is the square of an integer; in other words, it is the product 
 //of some integer with itself.
 
+//The tests will always use some integral number, so don't worry about 
+//that in dynamic typed languages.
+
