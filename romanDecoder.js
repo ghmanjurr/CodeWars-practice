920f2bb017afbb5845ba172898103f32a85aa070
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-10 16:53:15 -0700

    roman decoder codewars

--- romanDecoder.js
+++ romanDecoder.js
@@ -14,5 +14,12 @@
 
 solution('XXI'); // should return 21
 
-
+//Symbol    Value
+//I          1
+//V          5
+//X          10
+//L          50
+//C          100
+//D          500
+//M          1,000
 
