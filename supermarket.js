2bb383a3b3b5913f0e2cf8398bd3369bb0d2ed7d
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-09 23:41:51 -0700

    supermarket queue codewars

--- supermarket.js
+++ supermarket.js
@@ -16,3 +16,20 @@
 
 //The function should return an integer, the total time required.
 
+//IMPORTANT: Please look at the examples and clarifications below, 
+//to ensure you understand the task correctly :)
+
+//Examples:
+
+queueTime([5,3,4], 1)
+// should return 12
+// because when there is 1 till, the total time is just the sum of the times
+
+queueTime([10,2,3,3], 2)
+// should return 10
+// because here n=2 and the 2nd, 3rd, and 4th people in the 
+// queue finish before the 1st person has finished.
+
+queueTime([2,3,10], 2)
+// should return 12
+
