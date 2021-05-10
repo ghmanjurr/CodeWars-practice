4e43e52c04bc68fceafa2784f34fbf3cfffa7b33
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-10 16:50:48 -0700

    roman decoder codewars

--- romanDecoder.js
+++ romanDecoder.js
@@ -1,3 +1,8 @@
 //Roman Numerals Decoder
 
-//
+//Create a function that takes a Roman numeral as its argument and returns 
+//its value as a numeric decimal integer. You don't need to validate the form 
+//of the Roman numeral.
+
+
+
