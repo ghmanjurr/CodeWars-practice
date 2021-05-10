06f0c188facb8fbcd643f1304fbb82055637de9d
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-09 23:38:50 -0700

    supermarket queue codewars

--- supermarket.js
+++ supermarket.js
@@ -1,3 +1,6 @@
 //The Supermarket Queue
 
-//
+//There is a queue for the self-checkout tills at the supermarket. 
+//Your task is write a function to calculate the total time required 
+//for all the customers to check out!
+
