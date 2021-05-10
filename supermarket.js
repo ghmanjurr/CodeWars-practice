1dca9c0cf68d23d65a4671c03a8cd1c004eb33a5
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-09 23:39:40 -0700

    supermarket queue codewars

--- supermarket.js
+++ supermarket.js
@@ -4,3 +4,9 @@
 //Your task is write a function to calculate the total time required 
 //for all the customers to check out!
 
+//Input
+
+//customers: an array of positive integers representing the queue. 
+//Each integer represents a customer, and its value is the amount of 
+//time they require to check out.
+
