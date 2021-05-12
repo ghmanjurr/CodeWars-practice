b6d6a79c2c650fb7dda2daa468fd33aa391f8893
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-11 17:56:27 -0700

    rectangles into squares codewars

---
+++ rectanglesToSquares.js
@@ -0,0 +1,7 @@
+//Rectangle into Squares
+
+//The drawing below gives an idea of how to cut a given "true" rectangle 
+//into squares ("true" rectangle meaning that the two dimensions are different).
+
+//https://i.imgur.com/lk5vJ7sm.jpg
+
