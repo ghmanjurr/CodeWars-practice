a290b456684e6afe4f4eaf5cb01dde668d71a0ef
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-15 16:28:13 -0700

    valid braces codewars

--- braces.js
+++ braces.js
@@ -1,2 +1,6 @@
 //Valid Braces
 
+//Write a function that takes a string of braces, and determines if the 
+//order of the braces is valid. It should return true if the string is valid, 
+//and false if it's invalid.
+
