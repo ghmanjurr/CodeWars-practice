d6708103093e7ace587e1bc61d952c5bb6219e36
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-16 16:16:38 -0700

    complementary DNA codewars

---
+++ dna.js
@@ -0,0 +1,6 @@
+//Complementary DNA
+
+//Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of 
+//cells and carries the "instructions" for the development and functioning 
+//of living organisms.
+
