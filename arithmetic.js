7595f9b13a6006039a40c27e0f26feba5a8181cf
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-17 15:21:13 -0700

    arithmetic codewars'

--- arithmetic.js
+++ arithmetic.js
@@ -1,3 +1,5 @@
 //Make a function that does arithmetic!
 
-//
+//Given two numbers and an arithmetic operator (the name of it, as a string), 
+//return the result of the two numbers having that operator used on them.
+
