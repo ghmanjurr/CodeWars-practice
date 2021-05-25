74fd190c228c8f30fd94a91a3de1dcec0ed6dcea
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-24 23:29:13 -0700

    roman numerals codewars

--- romanNums.js
+++ romanNums.js
@@ -13,3 +13,12 @@
 
 //solution(1000); // should return 'M'
 
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
