c2f32c039aa2c373e89238cacea74f5586d4f406
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-26 23:06:12 -0700

    diamond string codewars

--- diamond.js
+++ diamond.js
@@ -1,3 +1,6 @@
 //Give me a diamond
 
-//
+//Jamie is a programmer, and James' girlfriend. She likes diamonds, 
+//and wants a diamond string from James. Since James doesn't know how 
+//to make this happen, he needs your help.
+
