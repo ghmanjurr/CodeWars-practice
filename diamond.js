62690d365ac08448800e54d9d9547fbe37671626
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-05-26 23:07:03 -0700

    diamond string codewars

--- diamond.js
+++ diamond.js
@@ -10,3 +10,6 @@
 //on the screen, using asterisk (*) characters. Trailing spaces should be 
 //removed, and every line must be terminated with a newline character (\n).
 
+//Return null/nil/None/... if the input is an even number or negative, as it 
+//is not possible to print a diamond of even or negative size.
+
