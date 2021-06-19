f7d7eb413635536191383c64185c2f4fe41a0f50
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-06-19 07:34:32 -0700

    training js 02 codewars

--- trainingjs02.js
+++ trainingjs02.js
@@ -10,3 +10,30 @@
 // value equal to 100. the function equal1 is completed, please refer to 
 // this example to complete the following functions.
 
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a=   //set number value to a
+  var b=   //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=   //set number value to a
+  var b=   //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a=   //set number value to a
+  var b=   //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=   //set number value to a
+  var b=   //set number value to b
+  return a%b;
+}
