7c6a3df7f4deff9abcaa94b9499c9fc1e3e99663
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-06-20 20:21:28 -0700

    codewars js training 03

--- trainingjs03.js
+++ trainingjs03.js
@@ -4,3 +4,13 @@
 
 // String can use operators +, Connect two or more strings together.
 
+// mission 1: I've create three function, and defined some global variables, 
+// please select some variables that can make up the name of the function, 
+// and return them(Please note the uppercase and lowercase letters are different).
+
+// When you have finished the work, click "Run Tests" to see if your code is 
+// working properly.
+
+// mission 2: After misson 1 finished. you should click "Attempt" to see my three 
+// questions, and write the answer in function answer1, answer2,answer3
+
