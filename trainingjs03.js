fbd3411d30bf7ff8ea5153b2706eec4bd0eb10ec
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-06-20 20:20:25 -0700

    codewars js training 03

---
+++ trainingjs03.js
@@ -0,0 +1,6 @@
+// In javascript, String is one of basic data types. Use "" or '' to 
+// declare a string variable. It can be letters:a,b,c,A,B,C, 
+// numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+
+// String can use operators +, Connect two or more strings together.
+
