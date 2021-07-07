8ea20418b2c278ffd6b28d212f6e16574374eb0f
Ghalib Manjurr <manjurr.ghalib@gmail.com>
2021-07-07 15:51:29 -0700

    codewars js training 04

--- trainingjs04.js
+++ trainingjs04.js
@@ -1,20 +1,22 @@
+var arr = [1,2,3]
+
 function getLength(arr){
     //return length of arr
-    return ?
+    return arr.length
   }
   function getFirst(arr){
     //return the first element of arr
-    return ?
+    return arr[0]
   }
   function getLast(arr){
     //return the last element of arr
-    return ?
+    return arr[arr.length-1]
   }
   function pushElement(arr){
     var el=1;
     //push el to arr
     
-    return arr
+    return arr.push(el)
   }
   function popElement(arr){
     //pop an element from arr
